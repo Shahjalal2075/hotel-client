5d924d8bec0a4974b3519b5f4e276be350fb98f1
Shahjalal2075 <shahjalal2075@gmail.com>
2023-11-17 00:40:17 +0600

    offer banner add

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,13 @@
+import AdsSlider from "../../Shared/AdsSlider/AdsSlider";
 
 const Home = () => {
     return (
         <div>
-            <h2>Home</h2>
+            <AdsSlider></AdsSlider>
+            
+            <h2 className=" text-center text-4xl font-bold text-black">Stunning Imagery</h2>
+            <h2 className=" text-center text-4xl font-bold text-black">User Testimonials</h2>
+            <h2 className=" text-center text-4xl font-bold text-black">News Seltter</h2>
         </div>
     );
 };
---
+++ src/Shared/AdsSlider/AdsSlider.jsx
@@ -0,0 +1,31 @@
+const AdsSlider = () => {
+    return (
+        <div>
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/0CyCRxx/ads1.png" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/PMzXssp/ads2.png" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/7XmVbqn/ads3.png" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AdsSlider;
