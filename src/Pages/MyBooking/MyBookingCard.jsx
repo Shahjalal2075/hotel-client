bcdf574d4b12277a01d4a991b3ea7cf77704ef3e
Shahjalal2075 <shahjalal2075@gmail.com>
2023-11-19 01:41:17 +0600

    booking cancel button activate

--- src/Pages/MyBooking/MyBookingCard.jsx
+++ src/Pages/MyBooking/MyBookingCard.jsx
@@ -1,9 +1,47 @@
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const MyBookingCard = ({room}) => {
 
-    const { cover,title,date,max } = room;
+
+    const { _id,cover,title,date,max } = room;
+
+    const handleCancel = () => {
+        Swal.fire({
+            title: 'Are you sure to Cancel Room?',
+            text: `${title} ${max} person(s) for ${date}`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            cancelButtonText: "No",
+            confirmButtonText: "Confirm!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/booking/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => {
+                        res.json()
+                    })
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Canceled!',
+                                `${title} Room has been Canceled.`,
+                                'success'
+                            )
+                        }
+                    })
+                setTimeout(() => {
+                    window.location.reload();
+                }, 600);
+            }
+        })
+
+    }
 
     return (
         <div className="card card-side bg-base-100 shadow-xl">
@@ -12,7 +50,7 @@ const MyBookingCard = ({room}) => {
                 <h2 className="text-xl font-bold ">{title}</h2>
                 <p className='text-base'><span className='font-bold'>Max. Guest:</span> {max} person(s)</p>
                 <p className='text-base font-bold'>{date}</p>
-                <button className='cursor-pointer bg-[#d33] text-lg rounded-2xl mt-2 text-center text-white px-2 py-1 font-semibold'>Cancel Room</button>
+                <button onClick={handleCancel} className='cursor-pointer bg-[#d33] text-lg rounded-2xl mt-2 text-center text-white px-2 py-1 font-semibold'>Cancel Room</button>
             </div>
         </div>
     );
